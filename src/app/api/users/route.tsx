f97990f29ac83efd5ef4a3fdb398242482b2b142
Bryanlaptop <bryan.chaine@gmail.com>
2023-09-27 13:35:20 +0200

    add method to get user, not up

--- src/app/api/users/route.tsx
+++ src/app/api/users/route.tsx
@@ -17,3 +17,31 @@ export async function POST(req: Request) {
         status: 201,
     });
 }
+
+export async function GET(
+    req: Request,
+    { params }: { params: { name: string } }
+) {
+    console.log("Requête:", req.url);
+    console.log("Params:", params);
+
+    try {
+        const users = await prisma.user.findMany({
+            where: {
+                name: params.name,
+            },
+        });
+
+        if (!users || users.length === 0) {
+            return NextResponse.json({ message: "User not found" });
+        }
+
+        return NextResponse.json({ users });
+    } catch (error) {
+        console.error("Error:", error);
+        return NextResponse.json(
+            { message: "Internal server error" },
+            { status: 500 }
+        );
+    }
+}
