19f3af5fa6a9e93f0b5e3b963a4af69d71470d5b
Bryanlaptop <bryan.chaine@gmail.com>
2023-10-10 10:03:12 +0200

    added get user by name and mail and update user

--- src/app/api/users/route.tsx
+++ src/app/api/users/route.tsx
@@ -1,8 +1,9 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+// post user route
 export async function POST(req: Request) {
     const body = await req.json();
     const newUser = await prisma.user.create({
@@ -18,17 +19,18 @@ export async function POST(req: Request) {
     });
 }
 
-export async function GET(
-    req: Request,
-    { params }: { params: { name: string } }
-) {
-    console.log("Requête:", req.url);
-    console.log("Params:", params);
+// get one user by name route
+export async function GET(request: NextRequest) {
+    const { searchParams } = new URL(request.url);
+    const name = searchParams.get("name");
+    const email = searchParams.get("email");
 
     try {
         const users = await prisma.user.findMany({
             where: {
-                name: params.name,
+                name:
+                    (name && { contains: name }) ||
+                    (email && { contains: email }),
             },
         });
 
@@ -44,4 +46,22 @@ export async function GET(
             { status: 500 }
         );
     }
+}
+
+export async function PUT(req: Request) {
+    const body = await req.json();
+    const user = await prisma.user.update({
+        where: {
+            id: body.id,
+        },
+        data: {
+            name: body.name,
+            picture: body.picture,
+            email: body.email,
+            password: body.password,
+        },
+    });
+    return NextResponse.json(user, {
+        status: 201,
+    });
 }
