13cf7046208393827ad11a7e9fb2a8d02d5985ed
Bryanlaptop <bryan.chaine@gmail.com>
2023-10-12 02:09:33 +0200

    add route api

--- src/app/api/note/route.tsx
+++ src/app/api/note/route.tsx
@@ -3,14 +3,14 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-// user create a note route author is user relation to note
+// TODO find solution for post note with a user log in (authorId)
 export async function POST(req: Request) {
     const body = await req.json();
     const newNote = await prisma.note.create({
         data: {
             title: body.title,
             content: body.content,
-            importanceIndex: 1,
+            importanceIndex: body.importanceIndex,
             authorId: body.authorId,
         },
     });
